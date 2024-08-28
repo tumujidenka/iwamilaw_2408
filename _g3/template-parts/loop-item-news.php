f4fd2632c48d6419fc0b52d0d878b7bdb2eab095
ryoto_hiji <ryotohiji.41@gmail.com>
2024-08-29 08:27:14 +0900

    ・お知らせ一覧ページ 大まかな実装

--- _g3/template-parts/loop-item-news.php
+++ _g3/template-parts/loop-item-news.php
@@ -1,14 +1,6 @@
-<table class="newsTable">
-    <tr>
-        <article class="col-12">
-            <td>
-                <?php echo esc_html( get_the_date() ); ?>
-            </td>
-            <td>
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?>
-                </a>
-            </td>
-        </article>
+<article class="col-12">
+    <tr class="newsRow">
+        <td class="newsDate"><?php echo esc_html( get_the_date() ); ?></td>    
+        <td class="newsTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
     </tr>
-</table>
+</article>
---
+++ _g3/template-parts/main-singular.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Singular main template
+ *
+ * @package Lightning G3
+ */
+
+if ( apply_filters( 'lightning_is_extend_single', false ) ) :
+	do_action( 'lightning_extend_single' );
+else :
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			$template = 'template-parts/entry-' . esc_attr( $post->post_name ) . '.php';
+			$return   = locate_template( $template );
+			if ( $return && get_post_type() !== $post->post_name ) {
+				locate_template( $template, true );
+			} else {
+				lightning_get_template_part( 'template-parts/entry', get_post_type() );
+			}
+
+
+		endwhile;
+	endif;
+endif;
